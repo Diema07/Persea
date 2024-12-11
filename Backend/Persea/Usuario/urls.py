6e72bff8813173bc14b72120c246f0bb81db2d0c
Diema07 <147446365+Diema07@users.noreply.github.com>
2024-12-11 19:40:09 +0000

    creacion Usuario

--- Backend/Persea/Usuario/urls.py
+++
@@ -1,8 +0,0 @@
-# Usuario/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Ruta de la página principal
-    path('detalle/<int:id>/', views.detalle, name='detalle'),  # Otra vista
-]
--- Backend/Persea/asgi.py
+++ Backend/Persea/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Persea.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Persea.settings")
 
 application = get_asgi_application()
