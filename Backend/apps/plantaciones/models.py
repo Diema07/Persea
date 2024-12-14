2503f39499c6fd7bd1a42ca0dd37d97b207ca1da
Marii-gati <marisolmorales2997@gmail.com>
2024-12-13 21:25:22 -0500

    Actualizacion modelos

--- Backend/apps/plantaciones/models.py
+++ Backend/apps/plantaciones/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Plantacion(models.Model):
-    nombre_parcela = models.CharField(max_length=100)
-    fecha_plantacion = models.DateField()
-    usuario = models.ForeignKey('usuarios.Usuario', on_delete=models.CASCADE, related_name='plantaciones')
+    nombreParcela = models.CharField(max_length=50)
+    fechaPlantacion = models.DateField()
+    estado = models.BooleanField(default=True)  # Define el estado como activo (True) por defecto
+    idUsuario = models.ForeignKey('usuarios.Usuario', on_delete=models.CASCADE, related_name='plantaciones')
 
     def __str__(self):
-        return self.nombre_parcela
+        return f"{self.nombreParcela} ({'Activa' if self.estado else 'Inactiva'})"
--- Backend/apps/usuarios/models.py
+++ Backend/apps/usuarios/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Usuario(models.Model):
-    id = models.AutoField(primary_key=True)  # ID único y automático
     nombreUsuario = models.CharField(max_length=150)
     correo = models.EmailField(unique=True)  # Correo electrónico único
     idGoogle = models.CharField(max_length=255, unique=True)  # ID de Google único y requerido
